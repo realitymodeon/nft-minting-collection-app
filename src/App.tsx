cd2cea32e8b40a3820e3d2041cdde4d24e01b830
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-08-23 04:46:35 +0300

    Client Testnet v1.0.0
    - +Style Components - +React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+//Route
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import * as ROUTES from "../src/contants/routes";
 
+//Pages
+import Home from "./pages/home";
 function App() {
   return (
-      <>
-          Hello World!
-      </>
+      <Router>
+          <Routes>
+              <Route path={ROUTES.HOME} element={<Home/>}/>
+          </Routes>
+      </Router>
   );
 }
 
