ea375e9d6facc8824ed2200ab2b75fc28930fa4d
Faru Nuri Sönmez <info@farunurisonmez.com>
2022-10-15 21:46:45 +0300

    Client Testnet v1.0.0
    Popup integration

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -5,10 +5,17 @@ import {Area, Background} from "./index.styled"
 
 import {Button} from "../button";
 
+type PopupButton = {
+    title:string;
+    action:() => void;
+}
+
 type Props = {
     show: boolean;
     close: () => void;
     title?: string;
+    desc?:string;
+    buttons: PopupButton[];
 }
 
 const Popup: React.FC<Props> = (props) => {
@@ -18,7 +25,16 @@ const Popup: React.FC<Props> = (props) => {
             <Background onClick={() => props.close()}/>
             <div className="content">
                 {props.title && <h3 className="title">{props.title}</h3>}
-                <Button connect onClick={() => console.log("Popup Click")}>Metamask</Button>
+                {props.desc && <p className="desc">{props.desc}</p>}
+                <>
+                    {props.buttons.map((button, index) => {
+                        return(
+                            <Button connect key={index} onClick={button.action}>
+                                {button.title}
+                            </Button>
+                        )
+                    })}
+                </>
             </div>
         </Area>
     )
